6b9ced253565056e3ac0abdf38208caf2ada92cf
xayon40-12 <touroux.nathan@gmail.com>
2020-03-17 18:33:11 +0100

    clean main and add test insted, add philox

--- src/lib.rs
+++ src/lib.rs
@@ -9,5 +9,85 @@ pub use dim::Dim;
 
 pub mod kernels;
 pub mod algorithms;
+pub mod philox;
 
 pub type Result<T> = ocl::Result<T>;
+
+
+#[cfg(test)]
+mod test {
+    use crate::Handler;
+    use crate::descriptors::{BufferDescriptor::*,KernelDescriptor::*};
+    use crate::kernels::Kernel;
+    use crate::Dim;
+
+    #[test]
+    fn simple_main() -> crate::Result<()> {
+        let src = "u[x] = p+x_size*1000+x*100;";
+
+        let num = 8;
+        let mut gpu = Handler::builder()?
+            .add_buffer("u", Len(0.0, num))
+            .create_kernel(Kernel {
+                name: "main",
+                src,
+                args: vec![Buffer("u"),Param("p",0.0)]
+            })
+            .build()?;
+
+        for i in 0..10 {
+            gpu.run("main",Dim::D1(num),vec![Param("p", i as f64)])?;
+            assert_eq!(gpu.get("u")?, (0..num).map(|j| i as f64 + num as f64*1000.0 + j as f64*100.0).collect::<Vec<_>>());
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn plus() -> crate::Result<()> {
+        let num = 8;
+        let mut gpu = Handler::builder()?
+            .add_buffer("a", Data((0..num).map(|i| i as f64).collect()))
+            .add_buffer("b", Len(2.0, num))
+            .add_buffer("dst", Len(0.0, num))
+            .load_kernel("plus")
+            .build()?;
+
+        gpu.run("plus",Dim::D1(num),vec![Buffer("a"),Buffer("b"),Buffer("dst")])?;
+        assert_eq!(gpu.get("dst")?, (0..num).map(|i| i as f64 + 2.0).collect::<Vec<_>>());
+
+        Ok(())
+    }
+
+    #[test]
+    fn times() -> crate::Result<()> {
+        let num = 8;
+        let mut gpu = Handler::builder()?
+            .add_buffer("a", Data((0..num).map(|i| i as f64).collect()))
+            .add_buffer("b", Len(2.0, num))
+            .add_buffer("dst", Len(0.0, num))
+            .load_kernel("times")
+            .build()?;
+
+        gpu.run("times",Dim::D1(num),vec![Buffer("a"),Buffer("b"),Buffer("dst")])?;
+        assert_eq!(gpu.get("dst")?, (0..num).map(|i| i as f64 * 2.0).collect::<Vec<_>>());
+
+        Ok(())
+    }
+
+    #[test]
+    fn sum() -> crate::Result<()> {
+        let num = 8;
+        let mut gpu = Handler::builder()?
+            .add_buffer("src", Data((0..num).map(|i| i as f64).collect()))
+            .add_buffer("dst", Len(0.0, num))
+            .load_algorithm("sum")
+            .build()?;
+
+        gpu.run_algorithm("sum",Dim::D1(num),vec![Buffer("src"),Buffer("dst")])?;
+        assert_eq!(gpu.get("dst")?[0], (0..num).map(|i| i as f64).fold(0.0,|i,a| i+a));
+
+        Ok(())
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,7 @@
-use gpgpu::Handler;
-use gpgpu::descriptors::{BufferDescriptor::*,KernelDescriptor::*};
-use gpgpu::kernels::Kernel;
-use gpgpu::Dim;
+use gpgpu::philox::*;
 
-fn main() -> gpgpu::Result<()> {
-    let src = "u[x] = p+x_size*1000+x*100;";
-
-    let num = 8;
-    let mut gpu = Handler::builder()?
-        .add_buffer("u", Data((0..num).map(|i| i as f64).collect()))
-        .add_buffer("it", Data((0..num).map(|i| i as f64).collect()))
-        .add_buffer("u2", Len(2.0, num))
-        .add_buffer("buf+", Len(0.0, num))
-        .add_buffer("buf*", Len(0.0, num))
-        .add_buffer("sum", Len(0.0, num))
-        .load_kernel("plus")
-        .load_kernel("times")
-        .load_algorithm("sum")
-        .create_kernel(Kernel {
-            name: "main",
-            src,
-            args: vec![Buffer("u"),Param("p",10.0)]
-        })
-        .build()?;
-
-    for i in 0..10 {
-        gpu.run("main",Dim::D1(num),vec![Param("p", i as f64)])?;
-        gpu.run_algorithm("sum",Dim::D1(num),vec![BufArg("it","src"),BufArg("sum","dst")])?;
-        gpu.run("plus",Dim::D1(num),vec![BufArg("u","a"),BufArg("u2","b"),BufArg("buf+","dst")])?;
-        gpu.run("times",Dim::D1(num),vec![BufArg("u","a"),BufArg("u2","b"),BufArg("buf*","dst")])?;
-        println!("main : {:?}", gpu.get("u")?);
-        println!("sum : {:?}", gpu.get("sum")?);
-        println!("plus : {:?}", gpu.get("buf+")?);
-        println!("times: {:?}", gpu.get("buf*")?);
+fn main() {
+    for i in 0..100 {
+        println!("{:?}", philox4x32([0,0,0,i],[0,0],7));
     }
-
-    Ok(())
 }

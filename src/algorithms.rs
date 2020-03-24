d541d8b3887a3514af9bbc71b35e2e89bc4956f7
xayon40-12 <touroux.nathan@gmail.com>
2020-03-24 18:13:12 +0100

    add moments algorithm (not verified)

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -69,5 +69,52 @@ pub fn algorithms<'a>() -> HashMap<&'static str,Algorithm<'a>> {
                 },
             ]
         },
+        Algorithm {
+            name: "moments",
+            callback: Rc::new(|h: &mut Handler, dim: Dim, desc: Vec<KernelArg>| {
+                if desc.len() < 4 || desc.len() > 5 {
+                    panic!("Algorithm \"moment\" takes 4 or 5 arguments, {} given.", desc.len());
+                }
+                let bufsrc = desc[0].clone();
+                let buftmp = desc[1].clone();
+                let bufsum = desc[2].clone();
+                let bufdst = desc[3].clone();
+                let num: u32 = if desc.len() == 5 {
+                    if let Param("n",U32(num)) = desc[5] {
+                        if num < 1 { panic!("There must be at least one moment calculated in \"moments\" algorithm."); }
+                        num
+                    } else {
+                        panic!("Fifth parameter of \"moment\" algorithm must be U32.");
+                    }
+                } else {
+                    4
+                };
+                let x = match dim {
+                    D1(x) => x,
+                    D2(x,y) => x*y,
+                    D3(x,y,z) => x*y*z
+                };
+
+                h.run_arg("times",D1(x),vec![bufsrc.clone(),bufsrc.clone(),buftmp.clone()])?;
+                h.run_algorithm("sum",D1(x),vec![buftmp.clone(),bufsum.clone()])?;
+                h.run_arg("move_0_to_i",D1(1),vec![bufsum.clone(),bufdst.clone(),Param("i",U64(0))])?;
+                h.set_arg("times",vec![buftmp.clone(),bufsrc.clone(),buftmp.clone()])?;
+                for i in 1..num {
+                    h.run("time",D1(x))?;
+                    h.run_algorithm("sum",D1(x),vec![buftmp.clone(),bufsum.clone()])?;
+                    h.run_arg("move_0_to_i",D1(1),vec![bufsum.clone(),bufdst.clone(),Param("i",U64(i as u64))])?;
+                }
+                h.run_arg("cdivide",D1(num as _),vec![bufdst.clone(),Param("c",F64(num as f64))])?;
+
+                Ok(())
+            }),
+            kernels: vec![
+                Kernel {
+                    name: "move_0_to_i",
+                    args: vec![KC::Buffer("src",EmT::F64),KC::Buffer("dst",EmT::F64),KC::Param("i",EmT::U64)],
+                    src: "dst[i] = src[0];"
+                },
+            ]
+        },
     ].into_iter().map(|a| (a.name,a)).collect()
 }

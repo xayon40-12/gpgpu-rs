9bf8784c6a066dace39ff286f5b91fb13a11bc59
xayon40-12 <touroux.nathan@gmail.com>
2020-12-15 20:26:37 +0100

    fix IntegratorParam update

--- examples/pde.rs
+++ examples/pde.rs
@@ -52,18 +52,18 @@ fn simple_int() -> gpgpu::Result<()> {
         .create_algorithm(create_euler_pde("simple",2.0/m as f64,vec![
                 SPDE{ dvar: "u".into(), expr: vec!["v[_i]".into()] },
                 SPDE{ dvar: "v".into(), expr: vec!["a".into()] },
-        ],None,vec![("a".into(),CF64)]))
+        ],None,vec![("a".into(),CF64),("t".into(),CF64)]))
         .build()?;
 
     let args = vec![("a".to_string(),F64(1.0))];
-    let mut ip = IntegratorParam { t: 0.0, swap: 0, args };
+    let mut ip = IntegratorParam { t: 0.0, increment_name: "t".into(), args };
     let bufs = ["u","swu","v","swv"];
     for _ in 0..m {
         gpu.run_algorithm("simple",D1(l),&[],&bufs,Mut(&mut ip))?;
     }
     println!("simple_int");
-    println!("u:   {:?}", gpu.get(bufs[ip.swap])?.VF64());
-    println!("v:   {:?}", gpu.get(bufs[2+ip.swap])?.VF64());
+    println!("u:   {:?}", gpu.get("u")?.VF64());
+    println!("v:   {:?}", gpu.get("v")?.VF64());
     Ok(())
 }
 
@@ -71,18 +71,18 @@ use std::io::Write;
 use std::time::SystemTime;
 fn diffusion_int() -> gpgpu::Result<()> {
     let l = 1<<18;
-    let t = 7.0e-3;
+    let t = 7.0e-2;
     let dt = 3e-7;
     let mut gpu = Handler::builder()?
         .add_buffer("u", Data(VF64((0..l).map(|i| i as _).collect())))
         .add_buffer("swu", Len(0.0.into(),l))
         .create_algorithm(create_euler_pde("diffusion",dt,vec![
                 SPDE{ dvar: "u".into(), expr: vec!["D*(u[(x+1)%x_size]-2*u[x]+u[(x-1)%x_size])*ivdx*ivdx".into()] },
-        ],None,vec![("D".into(),CF64),("ivdx".into(),CF64)]))
+        ],None,vec![("D".into(),CF64),("ivdx".into(),CF64),("t".into(),CF64)]))
         .build()?;
 
-    let args = vec![("D".to_string(),F64(1.0)),("ivdx".to_string(),F64(10.0))];
-    let mut ip = IntegratorParam { t: 0.0, swap: 0, args };
+    let args = vec![("D".to_string(),F64(2.0)),("ivdx".to_string(),F64(10.0))];
+    let mut ip = IntegratorParam { t: 0.0, increment_name: "t".into(), args };
     let bufs = ["u","swu"];
     let m = (t/dt) as usize;
     let start = SystemTime::now();
@@ -92,7 +92,7 @@ fn diffusion_int() -> gpgpu::Result<()> {
     }
     println!("diffusion_int");
     println!("{} s / {} steps / {} elements", SystemTime::now().duration_since(start).unwrap().as_millis() as f64/1000.0, m, l);
-    println!("u[0]: {:?} <-> {}", gpu.get_first(bufs[ip.swap])?.F64(), (l-1) as f64/2.0);
+    println!("u[0]: {:?} <-> {}", gpu.get_first("u")?.F64(), (l-1) as f64/2.0);
     Ok(())
 }
 
@@ -110,10 +110,10 @@ fn diffusion_int_vect() -> gpgpu::Result<()> {
                     "u[0+_i]-2*u[1+_i]+u[2+_i]".into(),
                     "u[1+_i]-2*u[2+_i]+u[0+_i]".into(),
                 ]},
-        ],None,vec![]))
+        ],None,vec![("t".into(),CF64)]))
         .build()?;
 
-    let mut ip = IntegratorParam { t: 0.0, swap: 0, args: vec![] };
+    let mut ip = IntegratorParam { t: 0.0, increment_name: "t".into(), args: vec![] };
     let bufs = ["u","swu"];
     let m = (t/dt) as usize;
     let start = SystemTime::now();
@@ -123,7 +123,7 @@ fn diffusion_int_vect() -> gpgpu::Result<()> {
     }
     println!("diffusion_int_vect");
     println!("{} s / {} steps / {} elements", SystemTime::now().duration_since(start).unwrap().as_millis() as f64/1000.0, m, l);
-    println!("u[0]: {:?} <-> 1", gpu.get_firsts(bufs[ip.swap],9)?.VF64());
+    println!("u[0]: {:?} <-> 1", gpu.get_firsts("u",9)?.VF64());
     Ok(())
 }
 
@@ -150,11 +150,11 @@ fn diffusion_int_pde_gen() -> gpgpu::Result<()> {
         })
         .create_algorithm(create_euler_pde("diffusion",dt,vec![
                 SPDE{ dvar: "u".into(), expr },
-        ],None,vec![("D".into(),CF64),("ivdx".into(),CF64)]))
+        ],None,vec![("D".into(),CF64),("ivdx".into(),CF64),("t".into(),CF64)]))
         .build()?;
 
     let args = vec![("D".to_string(),F64(2.0)),("ivdx".to_string(),F64(2.0))];
-    let mut ip = IntegratorParam { t: 0.0, swap: 0, args };
+    let mut ip = IntegratorParam { t: 0.0, increment_name: "t".into(), args };
     let bufs = ["u","swu"];
     let m = (t/dt) as usize;
     let start = SystemTime::now();
@@ -164,6 +164,6 @@ fn diffusion_int_pde_gen() -> gpgpu::Result<()> {
     }
     println!("diffusion_int_pde_gen");
     println!("{} s / {} steps / {} elements", SystemTime::now().duration_since(start).unwrap().as_millis() as f64/1000.0, m, l);
-    println!("u[0]: {:?} <-> {}", gpu.get_first(bufs[ip.swap])?.F64(), (l-1) as f64/2.0);
+    println!("u[0]: {:?} <-> {}", gpu.get_first("u")?.F64(), (l-1) as f64/2.0);
     Ok(())
 }

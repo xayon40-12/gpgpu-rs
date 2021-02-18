0ea45767280a69650b971c2c6c5b6a50a76948f8
xayon40-12 <touroux.nathan@gmail.com>
2021-02-18 19:55:14 +0100

    add fix_newton and nullIfNaN

--- src/functions.rs
+++ src/functions.rs
@@ -33,6 +33,12 @@ impl<'a> From<&Function<'a>> for SFunction {
     }
 }
 
+impl<'a> From<Function<'a>> for SFunction {
+    fn from (f: Function<'a>) -> Self {
+        (&f).into()
+    }
+}
+
 #[derive(Clone,Debug)]
 pub enum Needed<'a> {
     FuncName(&'a str),
@@ -53,7 +59,6 @@ impl<'a> From<&Needed<'a>> for SNeeded {
         }
     }
 }
-
 
 pub fn functions() -> HashMap<&'static str,Function<'static>> {
     vec![
@@ -122,5 +127,88 @@ pub fn functions() -> HashMap<&'static str,Function<'static>> {
                 ,
             needed: vec![],
         },
+        Function {
+            name: "nullIfNaN",
+            args: vec![FCParam("x",CF64)],
+            ret_type: Some(CF64),
+            src: "    if(x!=x) {{ return 0.0; }} else {{ return x; }}",
+            needed: vec![],
+        },
     ].into_iter().map(|f| (f.name,f)).collect()
+}
+
+pub fn null_if_nan(x: f64) -> f64 {
+    if x!=x { 0.0 } else { x }
+}
+
+/// fix_newton create an OpenCL function called 'name' that will take one parameter (the initial point) and uses the already defined (global scope) function 'f' to which it will search the fixed point using Newton's method.
+pub fn fix_newton<'a>(name: &'a str, f: &'a str, extra_param_f: &'a[&'a str], e: f64, max_iter: i32) -> SFunction {
+    let mut extra = extra_param_f.join(",");
+    if extra.len() > 0 { extra.insert(0,','); }
+    Function {
+        name: &name,
+        args: extra_param_f.iter().fold(vec![FCParam("x",CF64)], |mut acc,i| { acc.push(FCParam(i,CF64)); acc }),
+        ret_type: Some(CF64),
+        src: &format!(
+            "    const double e = {};
+    const double h = {};
+    const double invh = {};
+    double v = {f}(x{extra});
+    double vp = ({f}(x+h{extra})-v)*invh;
+    double d = 2*e;
+    int i;
+    for(i = 0; i<{max} && d>e; i++){{
+        v = {f}(x{extra});
+        vp = ({f}(x+h{extra})-v)*invh;
+        d = x;
+        x -= v/vp;
+        d = fabs(d-x);
+    }}
+    if (i=={max}) {{
+        return 0.0/0.0;
+    }}
+    return x;", e, e/10.0, 10.0/e, max=max_iter, f=f, extra=extra),
+        needed: vec![Needed::FuncName(&f)],
+    }.into()
+}
+
+
+#[test]
+fn function_test() -> crate::Result<()> {
+use crate::Handler;
+use crate::Dim;
+use crate::descriptors::{
+    BufferConstructor::*,
+    KernelArg::*,
+    VecTypes::*,
+    KernelConstructor::*,
+    ConstructorTypes::*,
+};
+use crate::kernels::Kernel;
+
+    let num = 1<<6;
+    let mut gpu = Handler::builder()?
+        .add_buffer("u", Data(VF64((0..num).map(|i| i as f64 - 1.0).collect())))
+        .load_function("nullIfNaN")
+        .create_function(Function {
+            name: "f",
+            args: vec![FCParam("x",CF64),FCParam("a",CF64)],
+            ret_type: Some(CF64),
+            src: "    return x*x - a;",
+            needed: vec![],
+        })
+        .create_function(fix_newton("fix", "f", &["a"], 1e-4, 1000))
+        .create_kernel(&Kernel {
+            name: "_main",
+            src: "    double a = u[x];
+    u[x] = nullIfNaN(floor(fix(a,a)*1000)/1000.0);",
+            args: vec![KCBuffer("u",CF64)],
+            needed: vec![],
+        })
+    .build()?;
+
+    gpu.run_arg("_main",Dim::D1(num),&[Buffer("u")])?;
+    assert_eq!(gpu.get("u")?.VF64(), (0..num).map(|j| null_if_nan((f64::sqrt(j as f64 -1.0)*1000.0).floor()/1000.0) ).collect::<Vec<_>>());
+
+    Ok(())
 }

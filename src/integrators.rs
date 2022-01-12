e4158d393fc47f55cffb3d436135b31eafa99299
xayon40-12 <touroux.nathan@gmail.com>
2022-01-12 17:08:34 +0100

    add between stages ordered eq steps

--- src/integrators.rs
+++ src/integrators.rs
@@ -13,10 +13,17 @@ use crate::Handler;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum STEP {
+    PDE,
+    EQPDE,
+    BETWEENPDE(u32),
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct SPDE {
     pub dvar: String,
     pub expr: Vec<String>, //one String for each dimension of the vectorial pde
-    pub is_eq: bool,
+    pub step: STEP,
 }
 
 pub struct IntegratorParam {
@@ -135,7 +142,7 @@ fn multistages_kernels(
         );
         let src_eq = format!(
             "    uint i = x+x_size*(y+y_size*z);\n    dst[i] = {}[i];",
-            &argnames[i]
+            &argnames[i] // TODO: optimize: use argnames[0] here and in multistages_algorithm for eq, then only alocate one buffer for them
         );
 
         needed.push(NewKernel(
@@ -176,10 +183,28 @@ fn multistages_algorithm(
                 format!("{}_{}", &name, &d.dvar),
                 d.dvar.clone(),
                 d.expr.len(),
-                d.is_eq,
+                d.step.clone(),
             )
         })
         .collect::<Vec<_>>();
+    let mut vars_ranges = vars.iter().enumerate().map(|(i,(_,_,_,s))| {
+        match s {
+            STEP::PDE | STEP::EQPDE | STEP::BETWEENPDE(0) => (0,i),
+            STEP::BETWEENPDE(j) => (*j,i),
+        }
+    }).collect::<Vec<_>>();
+    vars_ranges.sort();
+    let (_,vars_ranges) = vars_ranges.into_iter()
+    .fold((0,vec![vec![]]), |(c,mut a),(j,i)| {
+        if j == c {
+            let l = a.len()-1;
+            a[l].push(i);
+            (c,a)
+        } else {
+            a.push(vec![i]);
+            (j,a)
+        }
+    });
     let mut len = (if stages.len() > 1 { 2 } else { 1 } + stages.len()) * vars.len();
     let nb_pde_buffers = len;
     if let Some(ns) = &needed_buffers {
@@ -232,39 +257,44 @@ fn multistages_algorithm(
                     .map(|i| format!("src{}", i))
                     .collect::<Vec<_>>();
                 for s in 0..nb_stages {
-                    for i in (0..vars.len()).rev() {
-                        args[0] = BufArg(&bufs[nb_per_stages * i + (s + 1)], "dst");
-                        for i in 0..vars.len() {
-                            args[1 + i] = BufArg(
-                                &bufs[nb_per_stages * i + if s == 0 { 0 } else { tmpid }],
-                                &vars[i].1,
-                            );
+                    let mut pred = vec![];
+                    for r in &vars_ranges {
+                        for &i in r {
+                            args[0] = BufArg(&bufs[nb_per_stages * i + (s + 1)], "dst");
+                            for i in 0..vars.len() {
+                                args[1 + i] = BufArg(
+                                    &bufs[nb_per_stages * i + if (s == 0 && !pred.contains(&i)) || (s == nb_stages-1 && pred.contains(&i))  { 0 } else { tmpid }],
+                                    &vars[i].1,
+                                );
+                            }
+                            h.run_arg(&vars[i].0, dim, &args)?;
+                            args[time_id] = Param(
+                                increment_name,
+                                (*t + stages[s].iter().fold(0.0, |a, i| a + i) * dt).into(),
+                            ); // increment time for next stage
                         }
-                        h.run_arg(&vars[i].0, dim, &args)?;
-                        args[time_id] = Param(
-                            increment_name,
-                            (*t + stages[s].iter().fold(0.0, |a, i| a + i) * dt).into(),
-                        ); // increment time for next stage
-                        let mut stage_args = vec![
-                            BufArg(
-                                &bufs[nb_per_stages * i
-                                    + if s == nb_stages - 1 { 0 } else { tmpid }],
-                                "dst",
-                            ),
-                            BufArg(&bufs[nb_per_stages * i], "src"),
-                            Param("h", dt.into()),
-                        ];
-                        stage_args.extend(
-                            (0..s + 1)
-                                .map(|j| BufArg(&bufs[nb_per_stages * i + (j + 1)], &argnames[j])),
-                        );
-                        let is_eq = vars[i].3;
-                        let stage_name = &if is_eq {
-                            format!("{}_stage{}_eq", name, s)
-                        } else {
-                            format!("{}_stage{}", name, s)
-                        };
-                        h.run_arg(stage_name, D1(d * vars[i].2), &stage_args)?; // vars[i].2 correspond to the vectorial dim of the current pde
+                        r.iter().for_each(|i| pred.push(*i));
+                        for &i in r {
+                            let mut stage_args = vec![
+                                BufArg(
+                                    &bufs[nb_per_stages * i
+                                        + if s == nb_stages - 1 { 0 } else { tmpid }],
+                                    "dst",
+                                ),
+                                BufArg(&bufs[nb_per_stages * i], "src"),
+                                Param("h", dt.into()),
+                            ];
+                            stage_args.extend(
+                                (0..s + 1)
+                                    .map(|j| BufArg(&bufs[nb_per_stages * i + (j + 1)], &argnames[j])),
+                            );
+                            let step = &vars[i].3;
+                            let stage_name = &match step {
+                                STEP::PDE => format!("{}_stage{}", name, s),
+                                STEP::EQPDE | STEP::BETWEENPDE(_) => format!("{}_stage{}_eq", name, s),
+                            };
+                            h.run_arg(stage_name, D1(d * vars[i].2), &stage_args)?; // vars[i].2 correspond to the vectorial dim of the current pde
+                        }
                     }
                 }
 
